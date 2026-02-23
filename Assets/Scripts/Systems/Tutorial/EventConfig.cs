2ad6d592c0d3238378bcbdeade23f683df3aba81
karelinaira <165160303+karelinaira@users.noreply.github.com>
2026-02-23 21:39:47 +0800

    eventsSystem 2

--- Assets/Scripts/Systems/Tutorial/EventConfig.cs
+++ Assets/Scripts/Systems/Tutorial/EventConfig.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 using Data;
 using Data.Player;
-using Interfaces;
 using Zenject;
 using System;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using TriInspector;
 
 namespace Systems.Tutorial
@@ -15,14 +13,11 @@ namespace Systems.Tutorial
     public class EventConfig : ScriptableObject
     {
         [SerializeField, ReadOnly] private string _idNode = Guid.NewGuid().ToString();
-        public string Id => _idNode;
+        public string IdNode => _idNode;
 
-        [SerializeField] private string _scene;
-        [SerializeField] private EventConfig _nextElement;
-        [SerializeField] private SequenceElement _element;
-        [Inject] private GlobalData _globalData;
-
-        private int _completedTask;
+        public string Scene;
+        public EventConfig NextElement;
+        public SequenceElement Element;
 
         [Button("Copy")]
         private void Copy()
@@ -30,57 +25,9 @@ namespace Systems.Tutorial
             GUIUtility.systemCopyBuffer = _idNode;
         }
 
-        public void StartingActivate()
+        public void TaskCompleted()
         {
-           if (_globalData.Get<SavablePlayerData>().EventsState != _idNode)
-            {
-                if (_nextElement) _nextElement.StartingActivate();
-
-                if (_scene != SceneManager.GetActiveScene().name) return;
-                foreach(ISequenceAction action in _element.Actions)
-                {
-                    action.Invoke();
-                }
-            }
-            else
-            {
-                Activate();
-            }
-
-        }
-        public void Activate()
-        {
-            if (_scene != SceneManager.GetActiveScene().name) return;
-            
-            if (_element.Tasks.Count == 0) Invoke();
-            else
-            {
-                foreach(ITask task in _element.Tasks)
-                {
-                    task.Activate();
-                    task.OnComplete += TaskCompleted;
-                }
-            }
-        }
-
-        private void TaskCompleted()
-        {
-            _completedTask ++;
-            if (_completedTask == _element.Tasks.Count)
-            {
-                Invoke();
-            }
-        }
-        
-        public void Invoke()
-        {
-            foreach(ISequenceAction action in _element.Actions)
-            {
-                action.Invoke();
-            }
-
-            _globalData.Edit<SavablePlayerData>(data => data.EventsState = _nextElement.Id);
-            _nextElement.Activate();
+            EventsSystem.OnEventCompleted?.Invoke(this);
         }
     }
 }
--- Assets/Scripts/Systems/Tutorial/EventsSystem.cs
+++ Assets/Scripts/Systems/Tutorial/EventsSystem.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using TriInspector;
 using Zenject;
+using UnityEngine.SceneManagement;
+using Interfaces;
 
 namespace Systems.Tutorial
 {
@@ -13,25 +15,79 @@ namespace Systems.Tutorial
         [SerializeField] private EventConfig _startNode;
         [SerializeField] private EventConfig[] _eventConfigs;
 
+        public static Action<EventConfig> OnEventCompleted = delegate {};
+        private string _finishString = "finish";
+
         [Inject] private GlobalData _globalData;
-        [Inject]
-        private void Construct(DiContainer container)
-        {
-            foreach(EventConfig e in _eventConfigs)
-                container.Inject(e);
-        }
 
         private void Start()
         {
+            OnEventCompleted += Invoke;
             StartSystem();
+        }
+        private void OnDestroy()
+        {
+            OnEventCompleted -= Invoke;
         }
 
         private void StartSystem()
         {
             if (string.IsNullOrEmpty(_globalData.Get<SavablePlayerData>().EventsState)) {
-                _globalData.Edit<SavablePlayerData>(data => data.EventsState = _startNode.Id);
+                _globalData.Edit<SavablePlayerData>(data => data.EventsState = _startNode.IdNode);
             }
-           _startNode.StartingActivate();
+           StartingActivate(_startNode);
+        }
+
+        
+        private void StartingActivate(EventConfig config)
+        {
+            if (_globalData.Get<SavablePlayerData>().EventsState != config.IdNode)
+            {
+                if (config.NextElement) StartingActivate(config.NextElement);
+
+                if (config.Scene != SceneManager.GetActiveScene().name) return;
+                foreach(ISequenceAction action in config.Element.Actions)
+                {
+                    action.Invoke();
+                }
+            }
+            else
+            {
+                Activate(config);
+            }
+        }
+
+        private void Activate(EventConfig config)
+        {
+            if (config.Scene != SceneManager.GetActiveScene().name) return;
+            
+            if (config.Element.Tasks.Count == 0) Invoke(config);
+            else
+            {
+                foreach(ITask task in config.Element.Tasks)
+                {
+                    task.Activate();
+                    task.OnComplete += config.TaskCompleted;
+                }
+            }
+        }
+        
+        private void Invoke(EventConfig config)
+        {
+            foreach(ISequenceAction action in config.Element.Actions)
+            {
+                action.Invoke();
+            }
+
+            if (config.NextElement == null)
+            {
+                _globalData.Edit<SavablePlayerData>(data => data.EventsState = _finishString);
+            }
+            else
+            {
+                _globalData.Edit<SavablePlayerData>(data => data.EventsState = config.NextElement.IdNode);
+                Activate(config.NextElement);
+            }
         }
     }
 }
