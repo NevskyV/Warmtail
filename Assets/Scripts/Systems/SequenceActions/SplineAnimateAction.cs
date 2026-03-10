4bb92ab64bb33ab7c0dfcc385f2b139adfe5e829
karelinaira <165160303+karelinaira@users.noreply.github.com>
2026-03-10 20:41:46 +0800

    bbb

--- Assets/Scripts/Systems/SequenceActions/SplineAnimateAction.cs
+++ Assets/Scripts/Systems/SequenceActions/SplineAnimateAction.cs
@@ -11,6 +11,7 @@ namespace Systems.SequenceActions
         [SerializeField] private string _characterId;
         [SerializeField] private string _splineId;
         [SerializeField] private bool _move = true;
+        [SerializeField] private bool _loop = false;
         private EventsData _eventsData;
 
         public void SetEventsData(EventsData data)
@@ -23,6 +24,9 @@ namespace Systems.SequenceActions
         {
             var character = _eventsData.SceneObjects[_characterId].GetComponent<SplineAnimate>();
             character.Container = _eventsData.SceneObjects[_splineId].GetComponent<SplineContainer>();
+            if (_loop) character.Loop = SplineAnimate.LoopMode.Loop;
+            else character.Loop = SplineAnimate.LoopMode.Once;
+
             character.Restart(false);
             if (_move) character.Play();
         }
